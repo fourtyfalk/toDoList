9106521ec09e501483d82e4a7414c4a6f844fb12
fourtyfalk <falkkazmierski@web.de>
2025-04-28 08:39:40 +0200

    Projektdateien hinzufügen.

---
+++ toDoList.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace toDoList
+{
+    partial class toDoList
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inserting = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.showAllToDosButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(519, 20);
+            this.textBox1.TabIndex = 0;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ToDos Hinzufügen";
+            // 
+            // inserting
+            // 
+            this.inserting.Location = new System.Drawing.Point(675, 99);
+            this.inserting.Name = "inserting";
+            this.inserting.Size = new System.Drawing.Size(75, 23);
+            this.inserting.TabIndex = 2;
+            this.inserting.Text = "Hinzufügen";
+            this.inserting.UseVisualStyleBackColor = true;
+            this.inserting.Click += new System.EventHandler(this.inserting_Click);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(675, 398);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 4;
+            this.exit.Text = "Beenden";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(123, 137);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(519, 259);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(675, 352);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 6;
+            this.delete.Text = "Löschen";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // showAllToDosButton
+            // 
+            this.showAllToDosButton.Location = new System.Drawing.Point(675, 137);
+            this.showAllToDosButton.Name = "showAllToDosButton";
+            this.showAllToDosButton.Size = new System.Drawing.Size(75, 45);
+            this.showAllToDosButton.TabIndex = 7;
+            this.showAllToDosButton.Text = "Alle ToDos Anzeigen";
+            this.showAllToDosButton.UseVisualStyleBackColor = true;
+            this.showAllToDosButton.Click += new System.EventHandler(this.showAllToDosButton_Click);
+            // 
+            // toDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showAllToDosButton);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.inserting);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "toDoList";
+            this.Text = "ToDoListe";
+            this.Load += new System.EventHandler(this.toDoList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button inserting;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button showAllToDosButton;
+    }
+}
+
---
+++ toDoList.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace toDoList
+{
+    public partial class toDoList : Form
+    {
+        private SqlConnection cn;
+
+        public toDoList()
+        {
+            InitializeComponent();
+        }
+
+        private void toDoList_Load(object sender, EventArgs e)
+        {
+            //Datenbank wurden eingerichtet
+            cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\guhde\source\repos\toDoList\toDoListe.mdf;Integrated Security=True");
+            cn.Open();
+
+            // Hier wurden zwei Tabellen aufgefüllt
+            string insertBefehlEins = "INSERT INTO [dbo].[ErledigtTable] (Id, ErledigtJa, ErledigtNein) VALUES (@Id, @ErledigtJa, @ErledigtNein)";
+            string insertBefehlZwei = "INSERT INTO [dbo].[OffenTable] (Id, OffenJa, OffenNein) VALUES (@Id, @OffenJa, @OffenNein)";
+            string insertFilePath = "insertErledigtOffen.txt";
+
+            // Hier wird mit der If-Abfrage mehrere Datei Erstellungen zu den Inserts abgefangen
+            if (!File.Exists(insertFilePath))
+            {
+                using (SqlCommand insertCommandOne = new SqlCommand(insertBefehlEins, cn))
+                {
+                    insertCommandOne.Parameters.AddWithValue("@Id", 1);
+                    insertCommandOne.Parameters.AddWithValue("@ErledigtJa", 0);
+                    insertCommandOne.Parameters.AddWithValue("@ErledigtNein", 1);
+
+                }
+                using (SqlCommand insertCommandTwo = new SqlCommand(insertBefehlEins, cn))
+                {
+                    insertCommandTwo.Parameters.AddWithValue("@Id", 2);
+                    insertCommandTwo.Parameters.AddWithValue("@ErledigtJa", 1);
+                    insertCommandTwo.Parameters.AddWithValue("@ErledigtNein", 0);
+
+                }
+                using (SqlCommand insertCommandThree = new SqlCommand(insertBefehlZwei, cn))
+                {
+                    insertCommandThree.Parameters.AddWithValue("@Id", 1);
+                    insertCommandThree.Parameters.AddWithValue("@OffenJa", 1);
+                    insertCommandThree.Parameters.AddWithValue("@OffenNein", 0);
+
+                }
+                using (SqlCommand insertCommandFour = new SqlCommand(insertBefehlZwei, cn))
+                {
+                    insertCommandFour.Parameters.AddWithValue("@Id", 2);
+                    insertCommandFour.Parameters.AddWithValue("@OffenJa", 0);
+                    insertCommandFour.Parameters.AddWithValue("@OffenNein", 1);
+
+                }
+                File.WriteAllText(insertFilePath, "INSERTS wurden erfolgreich hinzugefügt.");
+            }
+            textBox1.KeyDown += textBox_PressEnter;
+        }
+
+        // Hier kann das ToDo auch mit der Entertaste hinzugefügt werden
+        private void textBox_PressEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                inserting.PerformClick();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                textBox1.Clear();
+            }
+        }
+        private void inserting_Click(object sender, EventArgs e)
+        {
+            // Hier wird der ToDo Text eingefügt ins GUI
+            checkedListBox1.Items.Add(textBox1.Text);
+            int eidee = 1;
+            int offenId = 1;
+            int erledigtId = 1;
+
+            // Hier wird in der Datenbank für die Tabelle ToDoListTable Werte eingefügt
+            SqlCommand cmdOne = new SqlCommand("INSERT INTO [dbo].[ToDoListTable] ([Id], [Aufgabe], [Erledigt_Id_Fk], [Offen_Id_Fk]) VALUES (@Id, @Aufgabe, @Erledigt_Id_Fk, @Offen_Id_Fk)", cn);
+            
+            // Hier wird abgefragt wie viele Werte in der Tabelle existieren
+            SqlCommand cmdTwo = new SqlCommand("SELECT * FROM [dbo].[ToDoListTable]", cn);
+            SqlDataReader redeLoewe = cmdTwo.ExecuteReader();
+
+            // Hiermit wird eine neue Id generiert
+            while (redeLoewe.Read())
+            {
+                eidee += 1;
+            }
+            
+            //Hier wird der zweite Befehl beendet
+            redeLoewe.Close();
+
+            // Hier kommen die Werte für den Insert Befehl rein
+            cmdOne.Parameters.AddWithValue("Id", eidee);
+            cmdOne.Parameters.AddWithValue("Aufgabe", textBox1.Text);
+            cmdOne.Parameters.AddWithValue("Erledigt_Id_Fk", erledigtId);
+            cmdOne.Parameters.AddWithValue("Offen_Id_Fk", offenId);
+
+            //Hier wird der erste Befehl ausgeführt
+            cmdOne.ExecuteNonQuery();
+            
+        }
+
+        // Hier gibts die zweite Möglichkeit die Anwendung zu beenden
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        // Hier werden die ToDos gelöscht und auf eine Andere Ebene geschoben
+        private void delete_Click(object sender, EventArgs e)
+        {
+            int foundId = 0;
+
+            // Hier zählt er alle Elemente auf
+            SqlCommand cmdfindId = new SqlCommand("SELECT * FROM [dbo].[ToDoListTable]", cn);
+
+            // Hier zählt er alle aktuellen ToDos auf
+            for (int findMe = 0; findMe < checkedListBox1.Items.Count; findMe++)
+            {
+
+                // Hier sucht er nachdem angekreuzten ToDos
+                if (checkedListBox1.GetItemChecked(findMe))
+                {
+
+                    // Hier geht er die Liste in der Datenbank durch
+                    SqlDataReader leseId = cmdfindId.ExecuteReader();
+                    while (leseId.Read())
+                    {
+                        //Hier zählt er immer die eine gesuchte Id hoch  
+                        foundId += 1;
+                        string findMyId = leseId["Aufgabe"].ToString();
+
+
+                        // Hier überprüft er das den ersten angekreuzten ToDo und das aktuell gesuchte
+                        // Hier dürfen keine Duplikate direkt hintereinander stehen, sonst findet er nur den Ersten und löscht ausschließlich nur den Ersten
+                        if (checkedListBox1.CheckedItems.Contains(findMyId) && checkedListBox1.GetItemChecked(findMe) )
+                        {
+                            break;
+                        }
+                    }
+
+                    // Hier wird die Aufzählung aller Elemente beendet
+                    leseId.Close();
+
+                    // Hier werden die Foreign Keys ausgetauscht mit der Id, die angekreuzt wurden
+                    SqlCommand cmd = new SqlCommand("UPDATE [dbo].[ToDoListTable] SET [Erledigt_Id_Fk] = 2, [Offen_Id_Fk] = 2 WHERE [Id] = @Id", cn);
+                    cmd.Parameters.AddWithValue("Id", foundId);
+                    cmd.ExecuteNonQuery();
+
+                    // Hier wird das angekreuzte ToDos aus der ToDo Liste gelöscht
+                    checkedListBox1.Items.RemoveAt(findMe);
+
+                    // Hier wird der Zähler um einen zurückgesetzt, weil sonst überspringt er den nächsten Angekreuzten ToDo
+                    findMe -= 1;
+
+                    // Hier wird die gesuchte Id wieder zurückgesetzt
+                    foundId = 0;
+                }
+            }
+        }
+
+
+        // Ist dafür da um in der CheckedListBox Elemente auszuwählen und anzukreuzen
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Hier werden alle aktiven ToDos angezeigt
+        private void showAllToDosButton_Click(object sender, EventArgs e)
+        {
+
+            // Hier startet er die Auflistung von neu
+            checkedListBox1.Items.Clear();
+
+            // Mit dem Befehl sucht er nach den aktiven ToDos
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [dbo].[ToDoListTable] WHERE  [Erledigt_Id_Fk] = 1 AND [Offen_Id_Fk] = 1", cn);
+            SqlDataReader rede = cmd.ExecuteReader();
+
+            // Hier geht er alle aktiven ToDos durch und zeigt Sie dir im GUI an
+            while (rede.Read())
+            {
+                string toDoAufgaben = rede["Aufgabe"].ToString();
+                checkedListBox1.Items.Add(toDoAufgaben);
+            }
+
+            // Hier beendet er das Durchlesen
+            rede.Close();
+        }
+    }
+}
